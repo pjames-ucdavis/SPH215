4f99a1bb409998ac32c616fb1383d747e70bd2bc
Peter James <pjames1@MacGT2G46M.hs.ucdhs.ucdavis.edu>
2025-04-10 21:12:42 -0600

    Big April 10 Commit

--- Create_sac.metro.R
+++ Create_sac.metro.R
@@ -12,3 +12,46 @@ write_rds(sac.metro,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Pub
 url <- "https://github.com/pjames-ucdavis/SPH215/raw/main/sac.metro.rds"
 download.file(url, destfile = "sac.metro.rds", mode = "wb")
 data <- readRDS("sac.metro.rds")
+
+#download Noli's park dataset
+
+download.file(url = "https://raw.githubusercontent.com/crd230/data/master/spatiallab.zip", destfile = "spatiallab.zip")
+unzip(zipfile = "spatiallab.zip")
+getwd()
+parks<-st_read("Parks.shp", stringsAsFactors = FALSE)
+write_rds(parks,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/parks.rds")
+
+# bring in sac city
+pl <- places(state = "CA", year = 2018, cb = FALSE)
+sac.city <- filter(pl, NAME == "Sacramento")
+
+#Merge data on income with sac.city
+ca.tracts <- get_acs(geography = "tract", 
+                  year = 2023,
+                  variables = c(medinc = "B19013_001"), 
+                  state = "CA",
+                  survey = "acs5",
+                  output = "wide",
+                  geometry = TRUE)
+glimpse(ca.tracts)
+glimpse(sac.city)
+tm_shape(ca.tracts) +
+  tm_polygons() + tm_shape(sac.city) +
+  tm_polygons(col = "blue") 
+
+
+sac.city.tracts.w = st_join(ca.tracts, sac.city, join = st_within, left=FALSE)
+
+tm_shape(sac.city.tracts.w) +
+  tm_polygons(col = "blue") +
+  tm_shape(sac.city) +
+  tm_borders(col = "red")
+
+sac.city.w <- st_join(ca.tracts, sac.city, join = st_within, left=FALSE)
+glimpse(sac.city.w)
+
+tm_shape(sac.city.w) +
+  tm_polygons(col = "blue") 
+
+
+write_rds(sac.city.w,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/sac.city.rds")
---
+++ clean_places.R
@@ -0,0 +1,16 @@
+# Read in PLACES data
+places_ca<-read_csv("'/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/PLACES_CA_Tracts_2024.csv'")
+glimpse(places_ca)
+alameda<-filter(places_ca, LocationName == "06001401700")
+glimpse(alameda)
+table(alameda$Short_Question_Text)
+table(alameda$MeasureId)
+
+# Pick just Physical Inactivity
+names(places_ca)
+places_ca_lpa<-filter(places_ca, MeasureId == "LPA")
+glimpse(places_ca_lpa)
+summary(places_ca_lpa$Data_Value)
+
+# Write .csv
+write_csv(places_ca_lpa,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/places_ca_lpa.csv")
