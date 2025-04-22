36b2827c9aa1d78915c42cbd80770b227707757b
Peter James <pjames1@MacGT2G46M.hs.ucdhs.ucdavis.edu>
2025-04-22 07:37:10 -0700

    April 22 Try Two

--- Small_NDVI.R
+++ Small_NDVI.R
@@ -1,6 +1,14 @@
-NDVI_raster = rast("NDVI_BayArea.tif")
+NDVI_raster = rast("/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/NDVI_BayArea.tif")
 
 ## Get summary of raster data
 NDVI_raster
-sf_rast<-crop(NDVI_raster, ext(-122.53, -122.3, 37.6191, 37.9))
-write_rds(sf_rast,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/sf_rast.rds")
+NDVI_rast<-crop(NDVI_raster, ext(-122.6, -122.2, 37.6, 38.1))
+
+tmap_mode("plot")
+
+NDVI_map = tm_shape(NDVI_rast) +
+  tm_raster(style = "cont") +
+  tm_legend(outside = TRUE)
+NDVI_map
+
+writeRaster(sf_rast,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/NDVI_rast.tif")
