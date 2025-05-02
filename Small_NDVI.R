92e705bbdf2ba62c11d32b0fde1cff91d4de1b8c
Peter James <pjames1@MacGT2G46M.hs.ucdhs.ucdavis.edu>
2025-05-02 14:59:54 -0400

    May 2

--- Small_NDVI.R
+++ Small_NDVI.R
@@ -1,3 +1,6 @@
+library(terra)
+library(tmap)
+
 NDVI_raster = rast("/Users/pjames1/Library/CloudStorage/GoogleDrive-nhpja@channing.harvard.edu/My Drive/2024 EH 249 Built Environment Nature and Health Course/Lectures/Lecture 2 GIS Lab/Lab Materials for R/Read-in Files /NDVI_BayArea.tif")
 
 ## Get summary of raster data
@@ -11,6 +14,6 @@ NDVI_map = tm_shape(NDVI_rast) +
   tm_legend(outside = TRUE)
 NDVI_map
 
-writeRaster(sf_rast,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/NDVI_rast.tif", overwrite=TRUE)
-writeRaster(sf_rast,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/NDVI_BayArea.tif", overwrite=TRUE)
+writeRaster(NDVI_rast,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/NDVI_rast.tif", overwrite=TRUE)
+#writeRaster(sf_rast,"/Users/pjames1/Dropbox/UC Davis Folders/SPH 215 GIS and Public Health/Github_Website/SPH215/NDVI_BayArea.tif", overwrite=TRUE)
 
